c62ddcbda21dc129b79a2ebf51d4b5cf257b5aad
Harsh Gupta <harshrp94@gmail.com>
2018-04-29 13:42:09 +0100

    Finalising the code

--- FC2/samTest.py
+++
@@ -1,7 +0,0 @@
-import csv
-airp = open('./data/airport.csv','r',encoding='utf-8')
-ap = csv.reader(airp,delimiter=",")
-airport = {}
-for col in ap:
-    airport[col[4]] = list(col)
-print(airport.get('WLS'))
